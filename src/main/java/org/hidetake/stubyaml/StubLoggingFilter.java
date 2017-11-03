f45435a83f3a455e378786b7f2e5177cf72c59df
Hidetake Iwata <int128@gmail.com>
2017-11-03 13:40:36 +0900

    Fix response body not shown in some cases

--- src/main/java/org/hidetake/stubyaml/StubLoggingFilter.java
+++ src/main/java/org/hidetake/stubyaml/StubLoggingFilter.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
@@ -15,10 +16,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
+ * Spring Web filter for logging request and response.
+ *
+ * @author Hidetake Iwata
  * @see org.springframework.web.filter.AbstractRequestLoggingFilter
  * @see ContentCachingRequestWrapper
  * @see ContentCachingResponseWrapper
@@ -27,6 +33,16 @@ import java.util.stream.Stream;
 @ConditionalOnProperty(value = "no-request-response-log", matchIfMissing = true)
 @Component
 public class StubLoggingFilter extends OncePerRequestFilter {
+    private static final List<MediaType> VISIBLE_TYPES = Arrays.asList(
+        MediaType.valueOf("text/*"),
+        MediaType.APPLICATION_FORM_URLENCODED,
+        MediaType.APPLICATION_JSON,
+        MediaType.APPLICATION_XML,
+        MediaType.valueOf("application/*+json"),
+        MediaType.valueOf("application/*+xml"),
+        MediaType.MULTIPART_FORM_DATA
+    );
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (isAsyncDispatch(request)) {
@@ -37,8 +53,8 @@ public class StubLoggingFilter extends OncePerRequestFilter {
     }
 
     protected void doFilterWrapped(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response, FilterChain filterChain) throws ServletException, IOException {
-        beforeRequest(request, response);
         try {
+            beforeRequest(request, response);
             filterChain.doFilter(request, response);
         }
         finally {
@@ -49,48 +65,62 @@ public class StubLoggingFilter extends OncePerRequestFilter {
 
     protected void beforeRequest(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
         if (log.isInfoEnabled()) {
-            val address = request.getRemoteAddr();
-            val queryString = request.getQueryString();
-            if (queryString == null) {
-                log.info("{}> {} {}", address, request.getMethod(), request.getRequestURI());
-            } else {
-                log.info("{}> {} {}?{}", address, request.getMethod(), request.getRequestURI(), queryString);
-            }
-            Collections.list(request.getHeaderNames()).forEach(headerName ->
-                Collections.list(request.getHeaders(headerName)).forEach(headerValue ->
-                    log.info("{}> {}: {}", address, headerName, headerValue)));
-            val content = request.getContentAsByteArray();
-            if (content.length > 0) {
-                log.info("{}>", address);
-                try {
-                    val contentString = new String(content, request.getCharacterEncoding());
-                    Stream.of(contentString.split("\r\n|\r|\n")).forEach(line -> log.info("{}> {}", address, line));
-                } catch (UnsupportedEncodingException e) {
-                    log.info("{}> [{} bytes body]", address, content.length);
-                }
-            }
-            log.info("{}>", address);
+            logRequestHeader(request, request.getRemoteAddr() + "|>");
         }
     }
 
     protected void afterRequest(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
         if (log.isInfoEnabled()) {
-            val address = request.getRemoteAddr();
-            val status = response.getStatus();
-            log.info("{}< {} {}", address, status, HttpStatus.valueOf(status).getReasonPhrase());
-            response.getHeaderNames().forEach(headerName ->
-                response.getHeaders(headerName).forEach(headerValue ->
-                    log.info("{}< {}: {}", address, headerName, headerValue)));
-            val content = response.getContentAsByteArray();
-            if (content.length > 0) {
-                log.info("{}<", address);
-                try {
-                    val contentString = new String(content, request.getCharacterEncoding());
-                    Stream.of(contentString.split("\r\n|\r|\n")).forEach(line -> log.info("{}< {}", address, line));
-                } catch (UnsupportedEncodingException e) {
-                    log.info("{}< [{} bytes body]", address, content.length);
-                }
+            logRequestBody(request, request.getRemoteAddr() + "|>");
+            logResponse(response, request.getRemoteAddr() + "|<");
+        }
+    }
+
+    private static void logRequestHeader(ContentCachingRequestWrapper request, String prefix) {
+        val queryString = request.getQueryString();
+        if (queryString == null) {
+            log.info("{} {} {}", prefix, request.getMethod(), request.getRequestURI());
+        } else {
+            log.info("{} {} {}?{}", prefix, request.getMethod(), request.getRequestURI(), queryString);
+        }
+        Collections.list(request.getHeaderNames()).forEach(headerName ->
+            Collections.list(request.getHeaders(headerName)).forEach(headerValue ->
+                log.info("{} {}: {}", prefix, headerName, headerValue)));
+        log.info("{}", prefix);
+    }
+
+    private static void logRequestBody(ContentCachingRequestWrapper request, String prefix) {
+        val content = request.getContentAsByteArray();
+        if (content.length > 0) {
+            logContent(content, request.getContentType(), request.getCharacterEncoding(), prefix);
+        }
+    }
+
+    private static void logResponse(ContentCachingResponseWrapper response, String prefix) {
+        val status = response.getStatus();
+        log.info("{} {} {}", prefix, status, HttpStatus.valueOf(status).getReasonPhrase());
+        response.getHeaderNames().forEach(headerName ->
+            response.getHeaders(headerName).forEach(headerValue ->
+                log.info("{} {}: {}", prefix, headerName, headerValue)));
+        log.info("{}", prefix);
+        val content = response.getContentAsByteArray();
+        if (content.length > 0) {
+            logContent(content, response.getContentType(), response.getCharacterEncoding(), prefix);
+        }
+    }
+
+    private static void logContent(byte[] content, String contentType, String contentEncoding, String prefix) {
+        val mediaType = MediaType.valueOf(contentType);
+        val visible = VISIBLE_TYPES.stream().anyMatch(visibleType -> visibleType.includes(mediaType));
+        if (visible) {
+            try {
+                val contentString = new String(content, contentEncoding);
+                Stream.of(contentString.split("\r\n|\r|\n")).forEach(line -> log.info("{} {}", prefix, line));
+            } catch (UnsupportedEncodingException e) {
+                log.info("{} [{} bytes content]", prefix, content.length);
             }
+        } else {
+            log.info("{} [{} bytes content]", prefix, content.length);
         }
     }
 
