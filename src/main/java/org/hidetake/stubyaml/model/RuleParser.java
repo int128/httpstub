1ffcedb1d7d1fa12091c8eedf73a216ff7e9f687
Hidetake Iwata <int128@gmail.com>
2017-11-24 18:15:23 +0900

    Remove commons-io to reduce size

--- src/main/java/org/hidetake/stubyaml/model/RuleParser.java
+++ src/main/java/org/hidetake/stubyaml/model/RuleParser.java
@@ -2,13 +2,13 @@ package org.hidetake.stubyaml.model;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.apache.commons.io.FileUtils;
 import org.hidetake.stubyaml.model.yaml.Rule;
 import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.error.YAMLException;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -21,7 +21,7 @@ public class RuleParser {
     private final Yaml yaml = new Yaml();
 
     public List<Rule> parse(File yamlFile) {
-        try (val yamlStream = FileUtils.openInputStream(yamlFile)) {
+        try (val yamlStream = new FileInputStream(yamlFile)) {
             val rules = yaml.loadAs(yamlStream, Rule[].class);
             if (rules == null) {
                 log.warn("No rules found in YAML file {}", yamlFile);
