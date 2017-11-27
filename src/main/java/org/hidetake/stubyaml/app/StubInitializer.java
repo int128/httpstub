76ca2877a911118012035862ee07f2448cc53c4e
Hidetake Iwata <int128@gmail.com>
2017-11-27 13:29:37 +0900

    Make configurable via properties

--- src/main/java/org/hidetake/stubyaml/app/StubInitializer.java
+++ src/main/java/org/hidetake/stubyaml/app/StubInitializer.java
@@ -5,11 +5,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.val;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 
 import java.io.File;
 import java.time.Duration;
 
+@ConfigurationProperties("stub")
 @RequiredArgsConstructor
 @Configuration
 public class StubInitializer implements InitializingBean {
@@ -19,12 +22,18 @@ public class StubInitializer implements InitializingBean {
     @Getter @Setter
     private String data = "data";
 
+    @Getter @Setter
+    private long intervalSec = 3;
+
     @Override
     public void afterPropertiesSet() {
+        Assert.hasText(data, "stub.data must have text");
+        Assert.state(intervalSec >= 0, "stub.intervalSec must be 0 or greater");
+
         val baseDirectory = new File(data);
         directoryWatcher.startThread(
             baseDirectory,
-            Duration.ofSeconds(3),
+            Duration.ofSeconds(intervalSec),
             () -> routeRegistrar.register(baseDirectory));
     }
 }
