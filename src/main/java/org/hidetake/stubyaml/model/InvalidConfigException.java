083b8c907e0b85460b96653779a47d5a41607140
Hidetake Iwata <int128@gmail.com>
2017-12-05 18:08:20 -0800

    Improve YAML error message

--- src/main/java/org/hidetake/stubyaml/model/InvalidConfigException.java
+++ src/main/java/org/hidetake/stubyaml/model/InvalidConfigException.java
@@ -6,6 +6,6 @@ import java.io.File;
 
 public class InvalidConfigException extends RuntimeException {
     public InvalidConfigException(File configFile, YAMLException e) {
-        super("Invalid config file: " + configFile, e);
+        super(String.format("Error in %s:\n%s", configFile, e));
     }
 }
